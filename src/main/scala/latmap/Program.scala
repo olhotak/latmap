027b22c4efc4f9c56395e9d9687def9de57c03d0
Ondrej Lhotak <olhotak@uwaterloo.ca>
2017-05-16 19:46:06 -0400

    tweaks/cleanup of Program interface

--- src/main/scala/latmap/Program.scala
+++ src/main/scala/latmap/Program.scala
@@ -2,24 +2,26 @@ package latmap
 
 trait Program {
   trait ProgVariable
-  type Variable <: ProgVariable
   trait ProgBodyElem
-  type BodyElem <: ProgBodyElem
   trait ProgAtom extends ProgBodyElem {
     def latMap: LatMap[_ <: Lattice]
     def keyVars: Seq[Variable]
     def latVar: Variable
   }
-  type Atom <: ProgAtom
   trait ProgConst extends ProgBodyElem {
-    def variable: ProgVariable
+    def variable: Variable
     def constant: Any
   }
-  type Const <: ProgConst
   trait ProgRule {
     def head: Atom
     def body: Seq[BodyElem]
   }
+
+  type Variable <: ProgVariable
+  type BodyElem <: ProgBodyElem
+  type Atom <: ProgAtom
+  type Const <: ProgConst
   type Rule <: ProgRule
+
   def rules: Seq[Rule]
 }
