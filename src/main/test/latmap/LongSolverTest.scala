4c9af7bed84dc461d76b484cb27617150db990dd
Ondrej Lhotak <olhotak@uwaterloo.ca>
2017-12-14 13:58:04 -0500

    add example SU program to LongSolverTest in addition to 470.lbm

--- src/main/test/latmap/LongSolverTest.scala
+++ src/main/test/latmap/LongSolverTest.scala
@@ -3,159 +3,165 @@ package latmap
 import org.scalatest.FunSuite
 
 class LongSolverTest extends FunSuite {
-  test("StrongUpdate tests") {
-    val p = API()
+  class SUProgram {
+    val api = API()
+    import api._
 
-    {
-      import p._
+    // Inputs
+    val AddrOf = relation(2)
+    val Copy = relation(2)
+    val Store = relation(3)
+    val Load = relation(3)
+    val CFG = relation(2)
+    val Multi = relation(1)
+    val Phi = relation(1)
+    val Clear = relation(1)
+    val FIStore = relation(3)
+    val FILoad = relation(3)
 
-      // Inputs
-      val AddrOf = relation(2)
-      val Copy = relation(2)
-      val Store = relation(3)
-      val Load = relation(3)
-      val CFG = relation(2)
-      val Multi = relation(1)
-      val Phi = relation(1)
-      val Clear = relation(1)
-      val FIStore = relation(3)
-      val FILoad = relation(3)
+    // Outputs
+    val Pt = relation(2)
 
-      // Outputs
-      val Pt = relation(2)
+    val pvar = variable()
+    val a = variable()
+    val b = variable()
+    val k = variable()
+    val l = variable()
+    val q = variable()
+    val t = latVariable(SULattice)
+    val z = latVariable(SULattice)
 
-      val pvar = variable()
-      val a = variable()
-      val b = variable()
-      val k = variable()
-      val l = variable()
-      val q = variable()
-      val t = latVariable(SULattice)
-      val z = latVariable(SULattice)
+    // Indexes
+    AddrOf(a,b).addIndex(a,b)
+    Copy(a,b).addIndex(a,b)
+    Copy(a,b).addIndex(a)
+    Store(a,b,k).addIndex(a,b,k)
+    Store(a,b,k).addIndex(b)
+    Store(a,b,k).addIndex(k)
+    Load(a,b,k).addIndex(a,b,k)
+    Load(a,b,k).addIndex(a)
+    Load(a,b,k).addIndex(k)
+    CFG(a,b).addIndex(a,b)
+    CFG(a,b).addIndex(a)
+    CFG(a,b).addIndex(b)
+    FIStore(a,b,k).addIndex(a)
+    FIStore(a,b,k).addIndex(b)
+    FILoad(a,b,k).addIndex(b)
 
-      // Indexes
-      AddrOf(a,b).addIndex(a,b)
-      Copy(a,b).addIndex(a,b)
-      Copy(a,b).addIndex(a)
-      Store(a,b,k).addIndex(a,b,k)
-      Store(a,b,k).addIndex(b)
-      Store(a,b,k).addIndex(k)
-      Load(a,b,k).addIndex(a,b,k)
-      Load(a,b,k).addIndex(a)
-      Load(a,b,k).addIndex(k)
-      CFG(a,b).addIndex(a,b)
-      CFG(a,b).addIndex(a)
-      CFG(a,b).addIndex(b)
-      FIStore(a,b,k).addIndex(a)
-      FIStore(a,b,k).addIndex(b)
-      FILoad(a,b,k).addIndex(b)
+    Pt(a,b).addIndex(a,b)
+    Pt(a,b).addIndex(a)
 
-      Pt(a,b).addIndex(a,b)
-      Pt(a,b).addIndex(a)
+    val SU = relation(2, SULattice)
+    SU(a,b,t).addIndex(a,b)
+    SU(a,b,t).addIndex(a)
 
-      val SU = relation(2, SULattice)
-      SU(a,b,t).addIndex(a,b)
-      SU(a,b,t).addIndex(a)
+    val PtH = relation(2)
+    PtH(a,b).addIndex(a,b)
+    PtH(a,b).addIndex(a)
+    val Kill = relation(1, SULattice)
 
-      val PtH = relation(2)
-      PtH(a,b).addIndex(a,b)
-      PtH(a,b).addIndex(a)
-      val Kill = relation(1, SULattice)
-
-      // Rules
-      // ----------
-      //
+    // Rules
+    // ----------
+    //
 
 
-      // AddrO
-      Pt(pvar,a) :- AddrOf(pvar,a)
+    // AddrO
+    Pt(pvar,a) :- AddrOf(pvar,a)
 
-      // Copy
-      Pt(pvar,a) :- (Copy(pvar,q), Pt(q,a))
+    // Copy
+    Pt(pvar,a) :- (Copy(pvar,q), Pt(q,a))
 
-      // Store
-      def toSingle(x: String) = SULattice.Single(x)
-      SU(l,a,z) :- (Store(l,pvar,q), Pt(pvar,a), Pt(q,b), T(z, toSingle, b))
+    // Store
+    def toSingle(x: String) = SULattice.Single(x)
+    SU(l,a,z) :- (Store(l,pvar,q), Pt(pvar,a), Pt(q,b), T(z, toSingle, b))
 
-      PtH(a,b) :- (Store(l,pvar,q), Pt(pvar,a), Pt(q,b))
-      PtH(a,b) :- (FIStore(pvar,q,l), Pt(pvar,a), Pt(q,b))
+    PtH(a,b) :- (Store(l,pvar,q), Pt(pvar,a), Pt(q,b))
+    PtH(a,b) :- (FIStore(pvar,q,l), Pt(pvar,a), Pt(q,b))
 
-      // Load
-      def filter(e: SULattice.Elem, p:String): Boolean = e match {
-        case SULattice.Bottom => false
-        case SULattice.Single(s) => p == s
-        case SULattice.Top => true
-      }
-
-      Pt(pvar,b) :- (Load(l,pvar,q), Pt(q,a), F(filter,t,b), PtH(a,b), SU(l,a,t))
-      Pt(pvar,b) :- (FILoad(pvar,q,l), Pt(q,a), PtH(a,b))
-
-      // Preserve
-      def killNot(a: String, e: SULattice.Elem): Boolean = e match {
-        case SULattice.Bottom => false
-        case SULattice.Single(s) => a != s
-        case SULattice.Top => true
-      }
-      SU(q,a,t) :- (CFG(l, q), SU(l,a,t), Multi(a))
-      SU(q,a,t) :- (CFG(l, q), SU(l,a,t), F(killNot,a,z), Kill(q, z))
-
-      // PtSU
-      SU(l,a,z) :- (Clear(l), PtH(a,b), T(z,toSingle,b))
-
-      // Kill
-      Kill(l,z) :- (Store(l,pvar,q), Pt(pvar,b), T(z,toSingle,b))
-      Kill(l, SULattice.Top) :- Phi(l)
-
-      // Example facts
-//      AddrOf("p","a") :- ()
-//      AddrOf("mb","b") :- ()
-//      Store("l1","p","mb") :- ()
-//      Load("l1","q","p") :- ()
-//      AddrOf("mc","c") :- ()
-//      Store("l3","p","mc") :- ()
-//      Load("l3","r","p") :- ()
-//      AddrOf("p2","d") :- ()
-//      AddrOf("mf","f") :- ()
-//      Store("l5","p2","mf") :- ()
-//      Phi("l6") :- ()
-//      Copy("p3","p") :- ()
-//      Copy("p3","p2") :- ()
-//      Load("l6","s","p3") :- ()
-//      AddrOf("me","e") :- ()
-//      Store("l7","p3","me") :- ()
-//      Load("l7","t","p3") :- ()
-//      CFG("l1","l3") :- ()
-//      CFG("l3","l5") :- ()
-//      CFG("l3","l6") :- ()
-//      CFG("l5","l6") :- ()
-//      CFG("l6","l7") :- ()
-
-      // Update the file name below to any .flix fact file that's in the test.latmap.testdata folder.
-      loadFactsFromFile(this.getClass.getResource("/470.lbm.flix").getPath, Map(
-        ("AddrOf", AddrOf),
-        ("Copy", Copy),
-        ("Load", Load),
-        ("Store", Store),
-        ("CFG", CFG),
-        ("Multi", Multi),
-        ("Phi", Phi),
-        ("Clear", Clear),
-        ("FILoad", FILoad),
-        ("FIStore", FIStore),
-        ("Pt", Pt),
-        ("SU", SU),
-        ("PtH", PtH),
-        ("Kill", Kill)
-      ))
-
-      p.solve()
-
-      // 469 facts should be in the output for 470.lbm.flix; update this number for other fact files.
-      assert(Pt.numFacts() == 469)
-
+    // Load
+    def filter(e: SULattice.Elem, p:String): Boolean = e match {
+      case SULattice.Bottom => false
+      case SULattice.Single(s) => p == s
+      case SULattice.Top => true
     }
 
-//    println(p.asInstanceOf[APIImpl].program)
+    Pt(pvar,b) :- (Load(l,pvar,q), Pt(q,a), F(filter,t,b), PtH(a,b), SU(l,a,t))
+    Pt(pvar,b) :- (FILoad(pvar,q,l), Pt(q,a), PtH(a,b))
+
+    // Preserve
+    def killNot(a: String, e: SULattice.Elem): Boolean = e match {
+      case SULattice.Bottom => false
+      case SULattice.Single(s) => a != s
+      case SULattice.Top => true
+    }
+    SU(q,a,t) :- (CFG(l, q), SU(l,a,t), Multi(a))
+    SU(q,a,t) :- (CFG(l, q), SU(l,a,t), F(killNot,a,z), Kill(q, z))
+
+    // PtSU
+    SU(l,a,z) :- (Clear(l), PtH(a,b), T(z,toSingle,b))
+
+    // Kill
+    Kill(l,z) :- (Store(l,pvar,q), Pt(pvar,b), T(z,toSingle,b))
+    Kill(l, SULattice.Top) :- Phi(l)
+
+    val predicateNames = Map(
+      ("AddrOf", AddrOf),
+      ("Copy", Copy),
+      ("Load", Load),
+      ("Store", Store),
+      ("CFG", CFG),
+      ("Multi", Multi),
+      ("Phi", Phi),
+      ("Clear", Clear),
+      ("FILoad", FILoad),
+      ("FIStore", FIStore),
+      ("Pt", Pt),
+      ("SU", SU),
+      ("PtH", PtH),
+      ("Kill", Kill)
+    )
+  }
+
+  test("StrongUpdate example") {
+    val program = new SUProgram
+
+    {
+      import program._, program.api._
+
+      // Example facts
+      AddrOf("p", "a") :- ()
+      AddrOf("mb", "b") :- ()
+      Store("l1", "p", "mb") :- ()
+      Load("l1", "q", "p") :- ()
+      AddrOf("mc", "c") :- ()
+      Store("l3", "p", "mc") :- ()
+      Load("l3", "r", "p") :- ()
+      AddrOf("p2", "d") :- ()
+      AddrOf("mf", "f") :- ()
+      Store("l5", "p2", "mf") :- ()
+      Phi("l6") :- ()
+      Copy("p3", "p") :- ()
+      Copy("p3", "p2") :- ()
+      Load("l6", "s", "p3") :- ()
+      AddrOf("me", "e") :- ()
+      Store("l7", "p3", "me") :- ()
+      Load("l7", "t", "p3") :- ()
+      CFG("l1", "l3") :- ()
+      CFG("l3", "l5") :- ()
+      CFG("l3", "l6") :- ()
+      CFG("l5", "l6") :- ()
+      CFG("l6", "l7") :- ()
+    }
+
+    program.api.solve()
+    assert(program.Pt.numFacts() == 16)
+  }
+
+  test("470.lbm") {
+    val program = new SUProgram
+    program.api.loadFactsFromFile(this.getClass.getResource("/470.lbm.flix").getPath, program.predicateNames)
+    program.api.solve()
+    assert(program.Pt.numFacts() == 469)
   }
 
 }
